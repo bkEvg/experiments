80fcea5dd874ec04e101709d627cd7da778265b3
Bauer Kirill <bk.evg@mail.ru>
2024-04-10 02:14:19 +0700

    rename

--- champoo/champoo/main.c
+++ champoo/champoo/main.c
@@ -1,19 +1,14 @@
 //
 //  main.c
-//  giraffe
+//  champoo
 //
 //  Created by Кирилл Бауэр on 10.04.2024.
 //
 
 #include <stdio.h>
-int multi(int x, int y);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n%d", multi(43, 5));
+    printf("Hello, World!\n");
     return 0;
-}
-
-int multi(int x, int y) {
-    return x * y;
 }
